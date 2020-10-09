3783843d72e258d271d049c1d0519ec3517a38b8
fredgoum <alfredgoumou@gmail.com>
2020-10-09 20:57:52 +0200

    google map integration

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>
+      <h1>Zoov</h1>
+    </div>
+    <br>
+    <gmap-map :center="center" :zoom="12" style="width:100%; height: 500px;">
+      <gmap-marker v-for="(marker, index) in markers" :key="'marker' + index" :position="marker.coordinates">
+      </gmap-marker>
+    </gmap-map>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GoogleMap",
+
+  data() {
+    return {
+      center: { lat: 45.508, lng: -73.587 },
+      markers: [{ coordinates: { lat: 45.508, lng: -73.587 } }, { coordinates: { lat: 45.500, lng: -73.5 } }],
+    };
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueGoogleMaps from "vue2-google-maps";
 
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyCJGxU5qVcWeC-qliA4_NEOUoHh0lYALhM",
+  }
+});
 
 new Vue({
   render: h => h(App),
